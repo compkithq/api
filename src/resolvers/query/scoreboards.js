994c98a166bd78ac63e29459244de2da349ec3da
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-01-27 12:57:32 +0000

    feat: Ensure a score and approproate rank are returned fro every athlete and workout
    Regardless of whether they've submitted a score

--- src/resolvers/query/scoreboards.js
+++ src/resolvers/query/scoreboards.js
@@ -3,57 +3,60 @@ const calculateScoreRank = require('../../utils/calculate-score-rank')
 module.exports = {
   scoreboard: async (root, { leaderboardId }, { db }) => {
     try {
-      const athletes = await db.Athlete.find({
+      const leaderboardAthletes = await db.Athlete.find({
         leaderboards: { $in: leaderboardId }
       }).exec()
 
-      const workouts = await db.Workout.find({
+      const leaderboardWorkouts = await db.Workout.find({
         leaderboard: { $eq: leaderboardId }
       }).exec()
 
-      const mappedAthletes = athletes.map(athlete => {
-        const { scores, ...rest } = athlete.toObject({
-          virtuals: true
-        })
+      const mappedAthletes = leaderboardAthletes.map(athlete => {
+        const { scores, ...rest } = athlete.toObject({ virtuals: true })
 
         return {
           ...rest,
           scores: async () => {
-            const workoutIds = workouts.map(({ id }) => id)
+            return leaderboardWorkouts.map(async workout => {
+              const athleteScore = await db.Score.findOne({
+                workout: { $eq: workout._id },
+                athlete: { $eq: athlete._id }
+              })
 
-            const athleteScores = await db.Score.find({
-              _id: { $in: scores },
-              workout: { $in: workoutIds }
-            }).exec()
-
-            return athleteScores.map(async score => {
               const allWorkoutScores = await db.Score.find({
-                workout: { $in: score.workout }
+                workout: { $in: workout._id }
               }).exec()
 
-              const { type: workoutType } = await db.Workout.findById(
-                score.workout
-              ).exec()
+              if (athleteScore) {
+                return {
+                  ...athleteScore.toObject({
+                    virtuals: true
+                  }),
+                  rank: calculateScoreRank({
+                    scores: allWorkoutScores,
+                    score: athleteScore.value,
+                    workoutType: workout.type
+                  })
+                }
+              }
+
+              const newScore = new db.Score({
+                value: 0,
+                workout: workout._id
+              })
 
               return {
-                ...score.toObject({
+                ...newScore.toObject({
                   virtuals: true
                 }),
-                rank: calculateScoreRank({
-                  scores: allWorkoutScores,
-                  score: score.value,
-                  workoutType
-                })
+                rank: leaderboardAthletes.length - allWorkoutScores.length
               }
             })
           }
         }
       })
 
-      return {
-        athletes: mappedAthletes,
-        workouts
-      }
+      return { athletes: mappedAthletes, workouts: leaderboardWorkouts }
     } catch (e) {
       return e
     }
