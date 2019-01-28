57c653b82ac12d8aa72d99849c45e0dbc89dde2c
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-01-28 13:53:23 +0000

    feat: Competition slug query (#32)
    * feat: Add getCompetitionBySlug query
    
    * refactor: Update competition, venue type schema
    
    * refactor: Remove unused venue resolver

--- src/db/competition.js
+++ src/db/competition.js
@@ -4,8 +4,14 @@ const { Schema } = mongoose
 module.exports = mongoose.model(
   'Competition',
   Schema({
-    name: 'String',
+    finalsDate: 'Date',
     leaderboards: [mongoose.Schema.Types.ObjectId],
+    name: 'String',
+    qualifiersEndDate: 'Date',
+    qualifiersStartDate: 'Date',
+    registrationEndDate: 'Date',
+    registrationStartDate: 'Date',
+    slug: 'String',
     venue: mongoose.Schema.Types.ObjectId
   })
 )
