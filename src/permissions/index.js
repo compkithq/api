a42a04b8025e648854a91bf7f4538e659c56a560
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-02-14 22:36:03 +0000

    feat: Add mutations to lock, unlock competition leaderboards

--- src/permissions/index.js
+++ src/permissions/index.js
@@ -2,6 +2,10 @@ const { shield } = require('graphql-shield')
 const rules = require('./rules')
 
 module.exports = shield({
+  Mutation: {
+    lockCompetitionLeaderboards: rules.isAuthenticated,
+    unlockCompetitionLeaderboards: rules.isAuthenticated
+  },
   Query: {
     me: rules.isAuthenticated
   }
