d13cb163f5e7b4deff81b31fd5492397f2ff6249
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-02-14 22:48:19 +0000

    fix: Actually export the lock leaderboard mutations

--- src/resolvers/mutation/index.js
+++ src/resolvers/mutation/index.js
@@ -1,11 +1,15 @@
 const athleteCompetitionRegistration = require('./athleteCompetitionRegistration')
 const auth = require('./auth')
 const createWorkoutScore = require('./createWorkoutScore')
+const lockCompetitionLeaderboards = require('./lockCompetitionLeaderboards')
+const unlockCompetitionLeaderboards = require('./unlockCompetitionLeaderboards')
 const updateWorkoutScore = require('./updateWorkoutScore')
 
 module.exports = {
   ...athleteCompetitionRegistration,
   ...auth,
   ...createWorkoutScore,
+  ...lockCompetitionLeaderboards,
+  ...unlockCompetitionLeaderboards,
   ...updateWorkoutScore
 }
