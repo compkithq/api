fec350a0fff1a34bfab3583448e8ba5303e7629d
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-02-27 21:43:57 +0000

    feat: Add rules to more mutations, queries

--- src/permissions/index.js
+++ src/permissions/index.js
@@ -3,10 +3,16 @@ const rules = require('./rules')
 
 module.exports = shield({
   Mutation: {
+    addAthletesToFinalsLeaderboard: rules.isAuthenticated,
+    athleteFinalsRegistration: rules.isAuthenticated,
+    athleteQualifiersRegistration: rules.isAuthenticated,
+    createWorkoutScore: rules.isAuthenticated,
     lockCompetitionLeaderboards: rules.isAuthenticated,
-    unlockCompetitionLeaderboards: rules.isAuthenticated
+    unlockCompetitionLeaderboards: rules.isAuthenticated,
+    updateWorkoutScore: rules.isAuthenticated
   },
   Query: {
+    getRelevantFinalsLeaderboards: rules.isAuthenticated,
     getRelevantQualifiersLeaderboard: rules.isAuthenticated,
     me: rules.isAuthenticated
   }
