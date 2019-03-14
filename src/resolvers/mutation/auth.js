3270731b6f0b74853ff371016bf4e195b13448e3
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-03-14 15:25:18 +0000

    feat: Add mutation to authenticate admin users (#68)

--- src/resolvers/mutation/auth.js
+++ src/resolvers/mutation/auth.js
@@ -2,6 +2,20 @@ const { IncorrectCredentialsError } = require('../../errors/auth')
 const { comparePassword, signToken } = require('../../utils')
 
 module.exports = {
+  authenticateAdmin: async (root, { email, password }, { db }) => {
+    const admin = await db.Admin.findOne({ email })
+
+    if (!admin) throw new IncorrectCredentialsError()
+
+    const passwordsMatch = await comparePassword(password, admin.password)
+
+    if (!passwordsMatch) throw new IncorrectCredentialsError()
+
+    return {
+      token: await signToken({ email, id: admin.id })
+    }
+  },
+
   authenticateAthlete: async (root, { email, password }, { db }) => {
     const athlete = await db.Athlete.findOne({ email })
 
