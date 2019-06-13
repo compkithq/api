7dfbc6d99afef749016627ca119d69f5c0d61f23
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-06-13 20:56:23 +0100

    fix: Add catch block identifier from handler

--- handlers/competitions.js
+++ handlers/competitions.js
@@ -3,7 +3,7 @@ const db = require('../db')
 module.exports = async (req, res) => {
   try {
     res.send(await db.Competition.find().exec())
-  } catch {
+  } catch (error) {
     res.status(500).json({
       status: '500',
       message: 'There was a problem processing your request'
