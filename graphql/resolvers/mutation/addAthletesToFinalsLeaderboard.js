fad8770818cf454edfc60cdf49d5308af01b510b
Jonathan Steele <jonathan@steele.pro>
2019-10-14 12:31:36 +0100

    refactor: Remove postmark client usage

--- graphql/resolvers/mutation/addAthletesToFinalsLeaderboard.js
+++ graphql/resolvers/mutation/addAthletesToFinalsLeaderboard.js
@@ -2,39 +2,12 @@ module.exports = {
   addAthletesToFinalsLeaderboard: async (
     root,
     { leaderboardId, athletes: ids },
-    { db, postmark }
+    { db }
   ) => {
     try {
-      const leaderboard = await db.FinalsLeaderboard.findByIdAndUpdate(
-        leaderboardId,
-        {
-          $push: { qualifiedAthletes: { $each: ids } }
-        }
-      )
-        .populate('competition')
-        .exec()
-
-      const athletes = await db.Athlete.find({ _id: { $in: ids } })
-
-      const {
-        competition: { name: competitionName },
-        name: leaderboardName
-      } = leaderboard
-
-      await postmark.sendEmailBatchWithTemplates(
-        athletes.map(({ email, name }) => ({
-          From: 'team@firstmeanseverything.com',
-          TemplateId: '10470851',
-          To: email,
-          TemplateModel: {
-            competitionName,
-            leaderboardName,
-            name
-          }
-        }))
-      )
-
-      return leaderboard
+      return await db.FinalsLeaderboard.findByIdAndUpdate(leaderboardId, {
+        $push: { qualifiedAthletes: { $each: ids } }
+      }).exec()
     } catch (e) {
       return e
     }
